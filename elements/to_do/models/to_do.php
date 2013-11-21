77f13722d39ce2b71f38c868235246c1236594dd
Nsl <nslmdrhr@gmail.com>
2013-11-21 17:06:00 +0100

    Modele Todo + logo

---
+++ elements/to_do/models/to_do.php
@@ -0,0 +1,13 @@
+<?php 
+	include('../../../tools/connection.php');
+	
+	class to_do{
+	
+	private $ID;
+	private $TITRE;
+	private $COMPLETE;
+	private $DUE_TIME;
+	private $PRIORITY;
+	private $USER;
+	
+	}
